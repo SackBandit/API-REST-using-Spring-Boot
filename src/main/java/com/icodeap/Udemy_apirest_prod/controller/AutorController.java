2b9d97e6075c9f4a0833a6872301ef6d8cb2c07a
Isaac Aarón Fierro Becerra <iafb2004@gmail.com>
2025-10-26 22:53:00 -0600

    Request and Response using the manual DTO pattern.”

--- src/main/java/com/icodeap/Udemy_apirest_prod/controller/AutorController.java
+++ src/main/java/com/icodeap/Udemy_apirest_prod/controller/AutorController.java
@@ -1,13 +1,16 @@
 package com.icodeap.Udemy_apirest_prod.controller;
 
+import com.icodeap.Udemy_apirest_prod.dto.AutorDTO;
 import com.icodeap.Udemy_apirest_prod.entity.Autor;
 import com.icodeap.Udemy_apirest_prod.service.AutorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/autores")
@@ -20,25 +23,79 @@ public class AutorController {
     }
     //http://localhost:8080/autores
     @PostMapping
-    public ResponseEntity<Autor> saveAutor(@RequestBody Autor autor) {
-        return new ResponseEntity<>(autorService.saveAutor(autor), HttpStatus.CREATED);
+    public ResponseEntity<AutorDTO> saveAutor(@RequestBody AutorDTO autorDTO) {
+        //DTO a Autor
+        Autor autor = new Autor();
+        autor.setNombre(autorDTO.getNombre());
+        autor.setApellido(autorDTO.getApellido());
+        autor.setTelefono(autorDTO.getTelefono());
+        //Guardar en la BD
+        Autor autorSaved = autorService.saveAutor(autor);
+
+        //Autor a AutorDTO
+        AutorDTO autorDTOSaved = new AutorDTO();
+        autorDTOSaved.setId(autorSaved.getId());
+        autorDTOSaved.setNombre(autorSaved.getNombre());
+        autorDTOSaved.setApellido(autorSaved.getApellido());
+        autorDTOSaved.setTelefono(autorSaved.getTelefono());
+
+        return new ResponseEntity<>(autorDTOSaved, HttpStatus.CREATED);
     }
 
     @GetMapping
-    public ResponseEntity<List<Autor>> findAll() {
-        return  ResponseEntity.ok(autorService.findAll());
+    public ResponseEntity<List<AutorDTO>> findAll() {
+        List<AutorDTO> autorDTOS = autorService.findAll().stream().map(
+                autor ->{
+                    AutorDTO autorDTO = new AutorDTO();
+                    autorDTO.setId(autor.getId());
+                    autorDTO.setNombre(autor.getNombre());
+                    autorDTO.setApellido(autor.getApellido());
+                    autorDTO.setTelefono(autor.getTelefono());
+                    return autorDTO;
+                }
+        ).toList();
+        if(autorDTOS.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return  ResponseEntity.ok(autorDTOS);
     }
     @GetMapping("/{id}")
-    public ResponseEntity<Autor> findById(@PathVariable Long id) {
+    public ResponseEntity<AutorDTO> findById(@PathVariable Long id) {
         Optional<Autor> autorOptional = autorService.findById(id);
-        return autorOptional.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+
+        return autorOptional.map(
+                autorDB -> {
+                    AutorDTO autorDTO = new AutorDTO();
+                    autorDTO.setId(autorDB.getId());
+                    autorDTO.setNombre(autorDB.getNombre());
+                    autorDTO.setApellido(autorDB.getApellido());
+                    autorDTO.setTelefono(autorDB.getTelefono());
+                    return ResponseEntity.ok(autorDTO);
+                }
+        ).orElseGet(
+                () -> ResponseEntity.notFound().build());
     }
 
     @PutMapping
-    public ResponseEntity<Autor> updateAutor(@RequestBody Autor autor) {
-        return autorService.updateAutor(autor).map(
-                ResponseEntity::ok
+    public ResponseEntity<AutorDTO> updateAutor(@RequestBody AutorDTO autorDTO) {
+        //DTO a Autor
+        Autor autor = new Autor();
+        autor.setId(autorDTO.getId());
+        autor.setNombre(autorDTO.getNombre());
+        autor.setApellido(autorDTO.getApellido());
+        autor.setTelefono(autorDTO.getTelefono());
 
+        return autorService.updateAutor(autor).map(
+                autorUpdated -> {
+                    AutorDTO autorDTOUpdated = new AutorDTO();
+
+                    autorDTOUpdated.setId(autorUpdated.getId());
+                    autorDTOUpdated.setNombre(autorUpdated.getNombre());
+                    autorDTOUpdated.setApellido(autorUpdated.getApellido());
+                    autorDTOUpdated.setTelefono(autorUpdated.getTelefono());
+
+                    return ResponseEntity.ok(autorDTOUpdated);
+                }
         ).orElseGet(
                 ()-> ResponseEntity.notFound().build());
     }
---
+++ src/main/java/com/icodeap/Udemy_apirest_prod/dto/AutorDTO.java
@@ -0,0 +1,50 @@
+package com.icodeap.Udemy_apirest_prod.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class AutorDTO {
+
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+
+    public AutorDTO() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+}
